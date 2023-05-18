32eb6022c915d495055f78ac73f0b531bc5f7393
Rybalovms <fish905@mail.ru>
2023-05-18 16:10:41 +0700

    Добавьте файлы проекта.

---
+++ TestVoprosi/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace TestVoprosi
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.nextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(400, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Location = new System.Drawing.Point(24, 66);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(35, 13);
+            this.questionLabel.TabIndex = 1;
+            this.questionLabel.Text = "label1";
+            this.questionLabel.Click += new System.EventHandler(this.questionLabel_Click_1);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(254, 253);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 6;
+            this.nextButton.Text = "Далее";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 303);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Test";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label questionLabel;
+    }
+}
+
---
+++ TestVoprosi/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TestVoprosi
+{
+    public partial class Test : Form
+    {
+        private List<Question> questions;
+        private int currentQuestionIndex;
+        private int correctAnswersCount;
+        public Test()
+        {
+            InitializeComponent();
+            LoadQuestionsFromFile("questions.txt"); // Загрузка вопросов из файла
+            InitializeTest();
+        }
+        private void LoadQuestionsFromFile(string fileName)
+        {
+            questions = new List<Question>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(fileName);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(';');
+                    string questionText = parts[0];
+                    List<string> answerOptions = new List<string>();
+
+                    for (int i = 1; i < parts.Length - 1; i++)
+                    {
+                        answerOptions.Add(parts[i]);
+                    }
+
+                    int correctAnswerIndex = int.Parse(parts[parts.Length - 1]);
+
+                    Question question = new Question(questionText, answerOptions, correctAnswerIndex);
+                    questions.Add(question);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке вопросов: " + ex.Message);
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeTest()
+        {
+            currentQuestionIndex = 0;
+            correctAnswersCount = 0;
+            progressBar1.Value = 0;
+
+            if (currentQuestionIndex < questions.Count)
+            {
+                Question currentQuestion = questions[currentQuestionIndex];
+                ShowQuestion(currentQuestion);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+        public class Question
+        {
+            public string Text { get; set; }
+            public List<string> AnswerOptions { get; set; }
+            public int CorrectAnswerIndex { get; set; }
+
+            public Question(string text, List<string> answerOptions, int correctAnswerIndex)
+            {
+                Text = text;
+                AnswerOptions = answerOptions;
+                CorrectAnswerIndex = correctAnswerIndex;
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            currentQuestionIndex++;
+            progressBar1.Value = (currentQuestionIndex * 100) / questions.Count;
+
+            ClearQuestion();
+            if (currentQuestionIndex < questions.Count)
+            {
+                Question currentQuestion = questions[currentQuestionIndex];
+                ShowQuestion(currentQuestion);
+            }
+            else
+            {
+                ShowResult();
+            }
+        }
+        private void ClearQuestion()
+        {
+            questionLabel.Text = string.Empty;
+
+            foreach (Control control in Controls)
+            {
+                if (control is RadioButton)
+                {
+                    Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+        }
+        private void ShowQuestion(Question question)
+        {
+           questionLabel.Text = question.Text;
+
+            for (int i = 0; i < question.AnswerOptions.Count; i++)
+            {
+                RadioButton radioButton = new RadioButton();
+                radioButton.Text = question.AnswerOptions[i];
+                radioButton.AutoSize = true;
+                radioButton.Location = new System.Drawing.Point(20, 100 + i * 30);
+                radioButton.Tag = i; 
+                radioButton.CheckedChanged += radioButton_CheckedChanged;
+
+                Controls.Add(radioButton);
+            }
+
+            nextButton.Enabled = false;
+        }
+        private void ShowResult()
+        {
+            double percentage = (correctAnswersCount * 100.0) / questions.Count;
+            MessageBox.Show("Результат: " + percentage.ToString("F2") + "% правильных ответов");
+
+            InitializeTest();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+        
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void questionLabel_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            int selectedAnswerIndex = (int)radioButton.Tag;
+
+            if (selectedAnswerIndex == questions[currentQuestionIndex].CorrectAnswerIndex)
+            {
+                correctAnswersCount++;
+            }
+
+            nextButton.Enabled = true;
+        }
+    }
+}
